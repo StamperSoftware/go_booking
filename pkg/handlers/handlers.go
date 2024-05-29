a96453ac0aed899cab8cdc529729739e71afd3b3
StamperSoftware <StamperSoftware@gmail.com>
2024-05-29 11:46:01 -0400

    init site

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,41 @@
+package handlers
+
+import (
+	"github.com/stampersoftware/go/booking/pkg/config"
+	"github.com/stampersoftware/go/booking/pkg/models"
+	"github.com/stampersoftware/go/booking/pkg/render"
+	"net/http"
+)
+
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+	render.RenderTemplate(w, "home.page.gohtml", &models.TemplateData{})
+}
+
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+
+	stringMap := make(map[string]string)
+	remoteIp := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIp
+
+	render.RenderTemplate(w, "about.page.gohtml", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
